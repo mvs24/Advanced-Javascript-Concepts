446b6d49f4bb8fd0e2a09994783292143acd08ed
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-07-23 10:49:18 +0200

    Add files via upload

---
+++ ExecutionContext/executionContext.js
@@ -0,0 +1,43 @@
+function printSomething() {
+  return "Something";
+}
+
+function print() {
+  printSomething();
+}
+
+function call() {
+  print();
+}
+
+call();
+
+
+
+
+// Execution Context
+
+// printSomething()
+// print()
+// call()
+// global()
+//.........................
+
+
+// print()
+// call()
+// global()
+//.........................
+
+// call()
+// global()
+//.........................
+
+// global()
+//.........................
+
+// program finished 
+
+
+
+
