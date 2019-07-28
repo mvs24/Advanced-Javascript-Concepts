6f350288d6d8775da66425e127465e037baefaea
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-07-28 14:31:19 +0200

    Update functionCurrying.js

--- FunctionInvocation/functionCurrying.js
+++ FunctionInvocation/functionCurrying.js
@@ -3,4 +3,7 @@ function multiply(a, b) {
 }
 
 let multiplyByTwo = multiply.bind(this, 2);
-console.log(multiplyByTwo(4));
+console.log(multiplyByTwo(4)); //first parameter is always 2
+
+let multiplyByTen = multiply.bind(this, 10);
+console.log(multiplyByTen(4)); //first parameter is always 10
