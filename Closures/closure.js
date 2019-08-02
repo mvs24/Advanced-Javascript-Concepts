6c81ad6eaa972e4a2a7339202d272ddf714842d9
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-08-02 22:15:46 +0200

    Closures

---
+++ Closures/closure.js
@@ -0,0 +1,17 @@
+function one() {
+  let string1 = "one";
+  return function two() {
+    let string2 = "two";
+    return function three() {
+      let string3 = "three";
+      console.log(string1, string2, string3);
+    };
+  };
+}
+
+one()()();
+
+const closure = string => number1 => number2 =>
+  console.log(`${string} ${number1} ${number2}`);
+
+closure("hi")(1)(10);
