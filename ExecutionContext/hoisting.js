2450c14cde483cc6ed72b231bbb559357ffcadd2
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-09-12 14:24:55 +0200

    Hoisting

--- ExecutionContext/hoisting.js
+++ ExecutionContext/hoisting.js
@@ -15,6 +15,7 @@ var addNumbers2 = function() {
     return 3+7;
 };
 
+
 // During the execution javascript engine knew that word will be a variable so initially word is undefined but addNumbers()
 // is a function and is fully hoisted
 // variables are partially hoisted  we hoist variables but not the right side (not their value)
