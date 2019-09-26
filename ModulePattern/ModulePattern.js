0e9689d67952edb8a3dbca45ec64576449282538
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-09-26 17:23:29 +0200

    Module Patterns

---
+++ ModulePattern/ModulePattern.js
@@ -0,0 +1,12 @@
+var modulePatrern = (function() {
+  let number1 = 4;
+  let number2 = 13;
+  function sum(num3) {
+    return number1 + number2 + num3;
+  }
+  return {
+    sum
+  };
+})();
+
+console.log(modulePatrern.sum(13));
