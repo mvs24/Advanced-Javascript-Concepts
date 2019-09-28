9201926c571064382f3ba8af49deebe1c3ba7367
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-09-28 12:31:31 +0200

    Update ModulePattern.js

--- ModulePattern/ModulePattern.js
+++ ModulePattern/ModulePattern.js
@@ -1,12 +1,15 @@
 let modulePattern = (function() {
+//   everything inside IIFE not being returned, is private to this IIFE
   let number1 = 4;
   let number2 = 13;
   function sum(num3) {
     return number1 + number2 + num3;
   }
   return {
+   // everything returned is going to be public
     sum
   };
 })();
 
-console.log(modulePatrern.sum(13));
+
+console.log(modulePattern.sum(13));
